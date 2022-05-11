629aba8e0826a9ef0372121dc53a09a36fc14245
JonasDeOliveira <jonas.doliver@gmail.com>
2022-05-11 09:41:36 -0300

    layout pronto

--- src/components/Todo/List/TodoList.js
+++ src/components/Todo/List/TodoList.js
@@ -1,12 +1,39 @@
-import React, { useState } from 'react'
+import React from 'react'
 import './TodoList.css'
+import { Container, ListGroup, Row, Col, Button } from 'react-bootstrap'
 
 function TodoList(props) {
 
-    return(
-        <>
-            <div>Lista</div>
-        </>
+    const renderTask = () => {
+        return (
+            <ListGroup.Item>
+                <Row className="itemTask">
+                    <Col xs={6} md={8}>
+                        Fazer algo
+                    </Col>
+                    <Col>
+                        <Button variant="info">
+                            Concluir
+                        </Button>
+                        <Button className="mx-3" variant="danger">
+                            Deletar
+                        </Button>
+                    </Col>
+                </Row>
+            </ListGroup.Item>
+        )
+    }
+
+    return (
+        <Container>
+            <Row>
+                <Col>
+                    <ListGroup variant="flush">
+                        {renderTask()}
+                    </ListGroup>
+                </Col>
+            </Row>
+        </Container>
     )
 }
 
